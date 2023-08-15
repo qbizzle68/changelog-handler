c36eb522a874ce41b97c5008f34539995f3a1e46
qbarnes <quinton.barnes68@gmail.com>
2023-08-14 23:18:03 -0500

    added version comparisons

--- changelog_handler/version.py
+++ changelog_handler/version.py
@@ -83,12 +83,85 @@ class SemanticVersion:
 
         return NotImplemented
 
+    def _compareVersion(self, rhs: 'SemanticVersion') -> int:
+        """Compare self to rhs with version numbering only. Returns -1 if self < rhs, 1 if self > rhs, 0 if they are
+        equal."""
+
+        if self._major < rhs._major:
+            return -1
+        elif self._major > rhs._major:
+            return 1
+
+        if self._minor < rhs._minor:
+            return -1
+        elif self._minor > rhs._minor:
+            return 1
+
+        if self._patch < rhs._patch:
+            return -1
+        elif self._patch > rhs._patch:
+            return 1
+
+        return 0
+
+    @staticmethod
+    def _compareToInt(lhs, rhs):
+        if lhs < rhs:
+            return -1
+        elif lhs > rhs:
+            return 1
+        return 0
+
+    @staticmethod
+    def _compareIdentifiers(rId: str, lId: str):
+        if rId.isdigit():
+            rId = int(rId)
+            if lId.isdigit():
+                lId = int(lId)
+                return SemanticVersion._compareToInt(rId, lId)
+            else:
+                return -1
+        elif lId.isdigit():
+            return 1
+        return SemanticVersion._compareToInt(rId, lId)
+
+    def _comparePreRelease(self, rhs: 'SemanticVersion') -> int:
+        if self._preRelease and not rhs._preRelease:
+            return -1
+        elif not self._preRelease and rhs._preRelease:
+            return 1
+        elif self._preRelease == rhs._preRelease:
+            return 0
+
+        rhsIdentifiers = self._preRelease.split('.')
+        lhsIdentifiers = rhs._preRelease.split('.')
+
+        for rId, lId in zip(rhsIdentifiers, lhsIdentifiers):
+            result = self._compareIdentifiers(rId, lId)
+            if result != 0:
+                return result
+
+        if len(rhsIdentifiers) < len(lhsIdentifiers):
+            return -1
+        elif len(rhsIdentifiers) > len(lhsIdentifiers):
+            return 0
+
     def __lt__(self, other: 'SemanticVersion') -> bool:
         if isinstance(other, SemanticVersion):
-            pass
+            cmpVersion = self._compareVersion(other)
+            if cmpVersion == -1:
+                return True
+            elif cmpVersion == 1:
+                return False
+
+            cmpPre = self._comparePreRelease(other)
+            if cmpPre == -1:
+                return True
+            return False
 
         return NotImplemented
 
     def toDict(self):
+        """Return a dict with all parts of a semantic version."""
         return {'major': self._major, 'minor': self._minor, 'patch': self._patch,
                 'pre_release': self._preRelease, 'build_metadata': self._build}
