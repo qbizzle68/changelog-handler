7df802a128f0d4c0522737a5350525b490e4af7d
qbarnes <quinton.barnes68@gmail.com>
2023-08-19 01:21:48 -0500

    import all test classes in test package

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,8 @@ import unittest
 
 from .versionTest import VersionTest
 from .patternTest import PatternTest
+from .changeTest import ChangeTest
+from .changelogTest import ChangelogTest
 
 if __name__ == '__main__':
     unittest.main()
