7333c771b24fdb6eaa1ed4a4c6923aabd0065c5e
qbarnes <quinton.barnes68@gmail.com>
2023-08-18 23:01:06 -0500

    date group not checked with unreleased

--- changelog_handler/_pattern.py
+++ changelog_handler/_pattern.py
@@ -11,7 +11,8 @@ SEMVAR_RAW = rf'[Vv]?(?P<version>{VERSION_CORE}(?:-{PRE_RELEASE})?(?:\+{BUILD})?
 SEMVAR = re.compile(f'{SEMVAR_RAW}$')
 DATE = r'(?P<date>\d{4}-\d{2}-\d{2})'
 URL = r'\s*\((?P<url>.+)\)'
-DELIMITER = re.compile(rf'##\s+(\[)?(?:(?P<unreleased>[Uu]nreleased)|{SEMVAR_RAW})(?(1)])(?:{URL})?\s+-\s+{DATE}')
+# DELIMITER = re.compile(rf'##\s+(\[)?(?:(?P<unreleased>[Uu]nreleased)|{SEMVAR_RAW})(?(1)])(?:{URL})?\s+-\s+{DATE}')
+DELIMITER = re.compile(rf'##\s+(\[)?(?:(?P<unreleased>[Uu]nreleased)|{SEMVAR_RAW})(?(1)])(?:{URL})?(?(3)\s+-\s+{DATE}|)')
 
 LINK = re.compile(rf'\[(?:(?P<unreleased>[Uu]nreleased)|{SEMVAR_RAW})]:\s+(?P<url>.+)')
 
